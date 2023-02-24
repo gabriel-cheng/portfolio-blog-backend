b39e676a8f69e5066ce77132aa6eb97fa8cc9a0e
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-02-24 02:08:43 -0300

    chore: testando deploy

--- src/app.ts
+++ src/app.ts
@@ -7,17 +7,17 @@ import MongoConnect from "./database/connect.database";
 import cors from "cors";
 import fs from "fs";
 const app = express();
-// const publicExists = fs.existsSync("public");
-// const uploadExists = fs.existsSync("public/uploads");
+const publicExists = fs.existsSync("public");
+const uploadExists = fs.existsSync("public/uploads");
 
 app.use(express.static("public"));
 
-// if(!publicExists) {
-//     fs.mkdirSync("public");
-// }
-// if(!uploadExists) {
-//     fs.mkdirSync("./public/uploads");
-// }
+if(!publicExists) {
+    fs.mkdirSync("public");
+}
+if(!uploadExists) {
+    fs.mkdirSync("./public/uploads");
+}
 
 app.use(cors());
 MongoConnect();
