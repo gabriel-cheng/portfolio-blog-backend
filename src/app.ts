7a650f12aeab739cc9f7767aab70358edc41d9e2
Gabrel Carvalho <gabrel_carvalho.contato@outlook.com>
2023-03-05 03:20:13 -0300

    chore: removendo códigos desnecessários

--- src/app.ts
+++ src/app.ts
@@ -5,19 +5,9 @@ import blogRouter from "./router/blog.router";
 import userRouter from "./router/user.router";
 import MongoConnect from "./database/connect.database";
 import cors from "cors";
-import fs from "fs";
 const app = express();
-const publicExists = fs.existsSync("public");
-const uploadExists = fs.existsSync("public/uploads");
 
 app.use(express.static("public"));
-
-if(!publicExists) {
-    fs.mkdirSync("public");
-}
-if(!uploadExists) {
-    fs.mkdirSync("./public/uploads");
-}
 
 app.use(cors());
 MongoConnect();
